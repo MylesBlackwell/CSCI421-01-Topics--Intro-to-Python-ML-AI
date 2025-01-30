a7afd1f1f2f26fcefde78f9d05de06e24dc986a5
Myles Blackwell <60078929+MylesBlackwell@users.noreply.github.com>
2025-01-29 19:11:31 -0500

    fixing main bug
    python was missing an element in installation. Next add comments and finish menu.

--- M03.py
+++ M03.py
@@ -7,20 +7,35 @@ Purpose: Working with Lists and Tuples with Input Validation
 daysOfTheWeek = ("Monday","Tuesday","Wednesday","Thursday","Friday",'Saturday',"Sunday")
 
 
-temperture = []
+temperature = {}
 for i in daysOfTheWeek:
     valid = False
-    while valid == False:
-     value = -21
-     try:
-        print("Enter the temperature for", i,":", end=" ")
-        value = int(input())
-     except:
-        print("Please enter an integer")
-    else:
-        if value > -20 and value < 120:
-            print("here")
-            valid = True
+    while not valid:
+        try:
+            value = int(input(f"Enter the temperature for {i}: "))
+            print("value is:" , value)
+            if -20 < value < 120:
+                temperature[i] = value
+                valid = True
+            else:
+                print("Temerture has to be greater than -20 degrees and less than 120 degrees.")
+
+        except:
+            print("Please enter an integer")
+
+menuStart = True
+
+while menuStart:
+    validMenuStart = False
+    while not validMenuStart:
+        valueMenuStart = input("\n\n\n Main Menu:\n would you like to change a temperature? y/n:")
+        if valueMenuStart == "y":
+            validMenuStart = True
+            for day,temp in temperature.items():
+                print(day,":\t",temp)
+                
+        elif valueMenuStart == "n":
+            validMenuStart = True
+            menuStart = False
         else:
-            valid = False
-            print("Temerture has to be greater than -20 degrees and less than 120 degrees.")
+            print("Please enter only 'y' or 'n'")
