6f68ac4caa58ef3e7c81469428fac95f19574bff
Myles Blackwell <mxblackwell10@gmail.com>
2025-02-16 19:57:23 -0500

    final commit

--- M04.py
+++ M04.py
@@ -3,6 +3,8 @@ Author: Myles Blackwell
 Date: 2-5-2025
 Purpose: Movie Recommendation System
 """
+import random
+import json
 
 #add movie function
 def addMovie(movie,genre):
@@ -15,22 +17,41 @@ def addMovie(movie,genre):
     except KeyError as e: 
         raise e
     
-#def removeMovie():
+def removeMovie(movie, genre):
+    try: #if genre is not found
+        if movie in movies[genre]: #if alrady in the genre
+            raise ValueError("movie already exit")
+        else: #adds movie to dict
+             movies[genre].remove(movie)
+             print(f'Movie "{movie}" has been to genre "{genre}')
+    except KeyError as e: 
+        raise e
 
-#def viewAllMovies():
+def viewAllMovies():
+    for genre in movies:
+            print(f"\n{genre}: ", end="")
+            movieList = movies[genre]
+            for movie in movieList:
+                print(movie, end=", ")
 
-#def searchMovie():
+def searchMovie(genre):
+    try:
+        print(f"\n{genre}: ", end = "")
+        for movie in movies[genre]:
+            print(movies, end=", ")
+    except KeyError as e:
+        raise e
 
-#def randomMovie()
+def randomMovie():
+    genre = random.choice(list(movies.keys()))
+    movie = random.choice(list(movies[genre]))
+    return movie
 
-#key is genere #value is a set containging movies
-movies = { 
-"horror": set([]),
-"Action": set(["Creed","Gladiator", "Without Remorse"]),
-"Drama" : set(["Just Mercy", "Forest Gump"]),
-"Romance": set([]),
-"Comedy" : set(["Friday"])
-}
+moviesFile = 'movies.txt' #file for saved movies
+
+global movies #key is genere #value is a set containging movies
+with open(moviesFile, 'r') as file:
+    movies = json.load(file)
 
 start = True
 print("Welcome to movie Recomendation System")
@@ -49,7 +70,7 @@ while start:
 
     if choice == 1: #Item 1 - Add movie
         try:
-            movie = input("Enter the movide name") #user inputs
+            movie = input("Enter the movie name") #user inputs
             genre = input("Enter the movie Sci-Fi")
 
             addMovie(movie,genre) #adds movie
@@ -58,3 +79,41 @@ while start:
         except KeyError:   #if genere already exist
             print("This genre was not found")
     
+    elif choice == 2: #Item 2 - Remove Movie
+        try:
+            movie = input("Enter the movie name") #user inputs
+            genre = input("Enter the movie Sci-Fi")
+
+            removeMovie(movie,genre)
+        except KeyError:   #if genere already exist
+            print("This genre or movie was not found")
+    
+    elif choice == 3: #Item 3 - View All Movie
+        print("Movies in the collection:")
+
+        viewAllMovies()
+
+    elif choice == 4: #Item 4 - Search Movies by Genere
+        print("Search movies by genre:")
+        try:
+            genre = input("Enter Genre: ")
+
+            searchMovie(genre)
+        except KeyError: 
+            print("This genre was not found")
+    
+    elif choice == 5: #Item 5 - Suggest a Random Moive 6
+        movie = randomMovie()
+        print(f'How about watching "{movie}"?')
+    
+    elif choice == 6 : #Item 6 - Ending
+        moviesConverted = {genre: list(movie) for genre, movie in movies.items()} #converts the sets into list because set is not compatable to JSON
+
+        with open(moviesFile, 'w') as file:
+            json.dump(moviesConverted, file)
+        
+        print("Saving movies and exiting... Goodbye!")
+        start = False
+    
+    else:
+        print("Please enter a choice 1-6")
